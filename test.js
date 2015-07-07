59c73df06274c9ab8d1621060e588794f2e1a38e
jonschlinkert <jon.schlinkert@sellside.com>
2015-07-07 04:15:26 -0400

    first commit

---
+++ test.js
@@ -0,0 +1,40 @@
+/*!
+ * dotdir-regex <https://github.com/regexps/dotdir-regex>
+ *
+ * Copyright (c) 2015 Jon Schlinkert.
+ * Licensed under the MIT License
+ */
+
+'use strict';
+
+var assert = require('assert');
+var re = require('./')();
+
+function match(str) {
+  var m = re.exec(str);
+  return m;
+}
+
+it('should match dotfiles', function () {
+  assert.equal(match('a/b/c/d/.git/')[1], '.git');
+  assert.equal(match('a/.git/c/')[1], '.git');
+  assert.equal(match('a/.git/')[1], '.git');
+  assert.equal(match('.git/')[1], '.git');
+  assert.equal(match('/.git/')[1], '.git');
+});
+
+it('should return false when the file is not a dot-directory', function () {
+  assert.equal(!!match('a/b/c/d/e.js'), false);
+  assert.equal(!!match('a/b.c.d/e.js'), false);
+  assert.equal(!!match('a/b.js'), false);
+  assert.equal(!!match('a/b/c/d/.git'), false);
+  assert.equal(!!match('a/.git'), false);
+  assert.equal(!!match('.git'), false);
+  assert.equal(!!match('/.git'), false);
+});
+
+it('should return true when the file is a dot-directory', function () {
+  assert.equal(!!match('a/.b/c/.git'), true);
+  assert.equal(!!match('a/.b/c/a.js'), true);
+  assert.equal(!!match('.git/foo'), true);
+});
